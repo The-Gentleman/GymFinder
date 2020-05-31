e6d360ff56a2e9b03bd8956c3745fadcfe110276
The-Gentleman <zraja4402@gmail.com>
2020-05-31 09:55:30 -0400

    exec

--- lib/GymFinder/cli.rb
+++ lib/GymFinder/cli.rb
@@ -1,7 +1,12 @@
 class GymFinder::CLI
     
-    def call 
-        puts "WHY ISNT THIS WORKING?"
+    def greeting
+        puts "Hi!"
+        select_gym
     end  
+
+    def select_gym
+        puts "Please select a gym:"
+    end 
 
 end 
