fc6b71ffb317521af0200fa5427348c3b7975e3e
The-Gentleman <zraja4402@gmail.com>
2020-06-04 10:19:07 -0400

    finished Scraper

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,17 +1,20 @@
  require_relative 'environment.rb'
  class Gym 
     @@gyms = []
-     attr_reader :name, :address, :num_of_stars 
+     attr_reader :name, :address, :workout_type, :num_of_stars 
 
-   def initialize(name, address, num_of_stars)
+   def initialize(name, address, workout_type, num_of_stars)
         @name = name 
         @address = address
         @num_of_stars = num_of_stars
+        @workout_type = workout_type
         @@gyms << self 
     end 
+
+    
 
     def self.gyms
       @@gyms 
     end 
-    puts 'Uhhhhh.....'
+
   end 
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,19 +4,22 @@ class Scraper
     def self.get_page
          Nokogiri::HTML(open("https://classpass.com/search/brooklyn/fitness-classes/6FFkjtq6yio"))
     end 
+    # CREATE EACH GYM AS ITS OWN HASH. THE NAME BEING THE HASH, AND 'KEYS' AND 'RATINGS' BEING VALUES.
+    def self.gym_info
+        gym_array = []
+        self.get_page.css("div._1LXXEz4ESHP2tFIArYDEAo").each do |section|
+            gym_hash = {}
+            gym_hash[:name] = section.css("h2.zeta.text--ellipsis").children.text
+            gym_hash[:address] = section.css("div._3oAhZ5phwNNDu5r1BFcnt1.text--ellipsis").text
+            gym_hash[:rating] = section.css("div.ratings.ratings--display._24_UanE3ZZOqnMtqOsSek6").text
+            gym_hash[:workout_type] = section.css("div._30U6zfyhNyRibTpCmWzMDd.text--ellipsis").text.gsub(/...\z/,'')
+            gym_array << gym_hash
+        end 
+        gym_array
+        # binding.pry
 
-    def self.gym_name
-        gym_name_array = []
-        name = self.get_page.css("div._3MJBIMPm13FBYtABhvSxRC")
-        name_iterator = name.css("h2.zeta.text--ellipsis").each do |title|
-            gym_name_array << title.children.text
-        end     
-        binding.pry
 
     end 
-    gym_name
 
-
-
-
+    gym_info
 end 
