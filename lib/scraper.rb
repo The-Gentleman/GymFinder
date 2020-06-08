df7153c4b873ad90c0e557e2f5602a54698183e3
The-Gentleman <zraja4402@gmail.com>
2020-06-08 10:26:47 -0400

    Refactored scraper method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,17 +4,13 @@ class Scraper
     end 
 
     def self.gym_info
-        gym_array = []
-
-        self.get_page.css("div._1LXXEz4ESHP2tFIArYDEAo").each do |section|
+        self.get_page.css("div._1LXXEz4ESHP2tFIArYDEAo").map do |section|
             gym_name = section.css("h2.zeta.text--ellipsis").children.text
             gym_address = section.css("div._3oAhZ5phwNNDu5r1BFcnt1.text--ellipsis").text
             gym_workout_type = section.css("div._30U6zfyhNyRibTpCmWzMDd.text--ellipsis").text.gsub(/...\z/,'')
             stars = section.css("div.ratings.ratings--display._24_UanE3ZZOqnMtqOsSek6").text.split(/[()]/)
             gym_num_of_stars = "#{stars[1]} people rated this gym #{stars[0]} stars!"
-            gym_array << Gym.new(gym_name, gym_address, gym_workout_type, gym_num_of_stars)
+            Gym.new(gym_name, gym_address, gym_workout_type, gym_num_of_stars)
         end 
-        
-        gym_array
     end 
 end 
